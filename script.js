34e6d222996d8420f73039890bdc22335895b565
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-08-27 19:44:16 -0300

    Finalizado protótipo

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+// Variables
 const brazilComponent = document.querySelector('#BR')
 const navegateButton = document.querySelector('#start')
+const popuptemplate = document.querySelector('#popup-template')
 
-brazilComponent.addEventListener('click', event => {
-    
-    const popuptemplate = document.querySelector('#popup-template')
+// Events
+brazilComponent.addEventListener('click', showPopup)
+
+// Functions
+function showPopup(){
     const node = document.importNode(popuptemplate.content, true)
 
-   document.body.appendChild(node)
+    document.body.appendChild(node)
 
-})
+    const popup = document.querySelector('.popup-container')
+
+    popup.addEventListener('click', event => {
+        popup.parentElement.removeChild(popup)
+    })
+}
